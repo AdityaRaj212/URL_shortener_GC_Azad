249e5221f61fc72541776279ce745875fe2e61da
Aditya Raj <adityaraj@pop-os.localdomain>
2024-03-05 08:07:18 +0530

    [feat]Added - history of all URLs shortened

--- config/urlshortener/urls.py
+++ config/urlshortener/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import home_view, redirect_url_view
+from .views import home_view, redirect_url_view, view_all_urls
 
 appname = "shortener"
 
 urlpatterns = [
     path("", home_view, name="home"),
     path('<str:shortened_part>', redirect_url_view, name="redirect"),
+    path('all-urls/', view_all_urls, name='all_urls'),
 ]
--- config/urlshortener/views.py
+++ config/urlshortener/views.py
@@ -19,15 +19,21 @@ def home_view(request):
     
     elif request.method == 'POST':
         used_form = ShortenerForm(request.POST)
-
         if used_form.is_valid():
             shortened_object = used_form.save()
             new_url = request.build_absolute_uri('/') + shortened_object.short_url
 
             long_url = shortened_object.long_url
 
+            times = shortened_object.times_followed
+
+            all_urls = Shortener.objects.all()
+
             context['new_url'] = new_url
             context['long_url'] = long_url
+            context['no_of_times'] = times
+            context['all_urls'] = all_urls
+
 
             return render(request,template,context)
         
@@ -42,3 +48,8 @@ def redirect_url_view(request, shortened_part):
         return HttpResponseRedirect(shortener.long_url)
     except:
         raise Http404('Sorry this link is broken :()')
+    
+def view_all_urls(request):
+    all_urls = Shortener.objects.all()
+    template = 'urlshortener/home.html'
+    return render(request,template,{'all_urls':all_urls})
